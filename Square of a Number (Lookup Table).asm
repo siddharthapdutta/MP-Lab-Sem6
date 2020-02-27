9eff9c728caa29636ecb0cca4224cc63cb2c9967
Siddhartha Dutta <siddharthadutta@protonmail.com>
2020-02-27 12:18:53 +0530

    Experiment-11

--- Square of a Number (Lookup Table).asm
+++ Square of a Number (Lookup Table).asm
@@ -1,10 +1,10 @@
 // Square of a Number using Lookup Table
 # ORG 2001
-# DB 01,04,09,10,19,24,31,40,51,64,79,90,A9,C4
+# DB 01,04,09,10,19,24,31,40,51,64,79,90,A9,C4,E1
 
 // Number is Stored in 3000H
 # ORG 3000
-# DB 08
+# DB 0F
 
 # ORG 0000
 LXI H, 2000H
